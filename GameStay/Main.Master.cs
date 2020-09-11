270919f4eec66267d610b2b582df94e8a272c00f
kshjaing <48698567+kshjaing@users.noreply.github.com>
2020-09-11 16:43:48 +0900

    마스터페이지 호버링 애니메이션(반쪽완성)

--- GameStay/Main.Master.cs
+++ GameStay/Main.Master.cs
@@ -11,12 +11,16 @@ namespace GameStay
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            img_store.Attributes["style"] = "visibility: visible";
+            img_library.Attributes["style"] = "visibility: hidden";
+            img_community.Attributes["style"] = "visibility: hidden";
+            img_profile.Attributes["style"] = "visibility: hidden";
         }
 
         protected void StoreMenu_OnClick(object sender, EventArgs e)
         {
             Response.Redirect("Store_Main.aspx");
+           
         }
         protected void LibraryMenu_OnClick(object sender, EventArgs e)
         {
@@ -25,10 +29,12 @@ namespace GameStay
         protected void CommunityMenu_OnClick(object sender, EventArgs e)
         {
             Response.Redirect("Community.aspx");
+            
         }
         protected void ProfileMenu_OnClick(object sender, EventArgs e)
         {
             Response.Redirect("Profile.aspx");
+            
         }
     }
 }
