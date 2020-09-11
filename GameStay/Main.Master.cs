185623968060b3d5921e84a211831fa3181dccf3
kshjaing <48698567+kshjaing@users.noreply.github.com>
2020-09-11 17:17:59 +0900

    마스터페이지 호버링 애니메이션 완성

--- GameStay/Main.Master.cs
+++ GameStay/Main.Master.cs
@@ -11,16 +11,40 @@ namespace GameStay
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            img_store.Attributes["style"] = "visibility: visible";
-            img_library.Attributes["style"] = "visibility: hidden";
-            img_community.Attributes["style"] = "visibility: hidden";
-            img_profile.Attributes["style"] = "visibility: hidden";
+            if(Request.Url.ToString().Contains("Store_Main.aspx"))
+            {
+                img_store.Style["visibility"] = "visible";
+                img_library.Style["visibility"] = "hidden";
+                img_community.Style["visibility"] = "hidden";
+                img_profile.Style["visibility"] = "hidden";
+            }
+            else if (Request.Url.ToString().Contains("Library.aspx"))
+            {
+                img_store.Style["visibility"] = "hidden";
+                img_library.Style["visibility"] = "visible";
+                img_community.Style["visibility"] = "hidden";
+                img_profile.Style["visibility"] = "hidden";
+            }
+            else if (Request.Url.ToString().Contains("Community.aspx"))
+            {
+                img_store.Style["visibility"] = "hidden";
+                img_library.Style["visibility"] = "hidden";
+                img_community.Style["visibility"] = "visible";
+                img_profile.Style["visibility"] = "hidden";
+            }
+            else if (Request.Url.ToString().Contains("Profile.aspx"))
+            {
+                img_store.Style["visibility"] = "hidden";
+                img_library.Style["visibility"] = "hidden";
+                img_community.Style["visibility"] = "hidden";
+                img_profile.Style["visibility"] = "visible";
+            }
         }
 
         protected void StoreMenu_OnClick(object sender, EventArgs e)
         {
             Response.Redirect("Store_Main.aspx");
-           
+
         }
         protected void LibraryMenu_OnClick(object sender, EventArgs e)
         {
@@ -29,12 +53,10 @@ namespace GameStay
         protected void CommunityMenu_OnClick(object sender, EventArgs e)
         {
             Response.Redirect("Community.aspx");
-            
         }
         protected void ProfileMenu_OnClick(object sender, EventArgs e)
         {
             Response.Redirect("Profile.aspx");
-            
         }
     }
 }
