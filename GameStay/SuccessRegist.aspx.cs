ac901e4be50bf0ae61342566c6c6e1d20f322171
kshjaing <48698567+kshjaing@users.noreply.github.com>
2020-10-05 23:48:36 +0900

    .

--- GameStay/SuccessRegist.aspx.cs
+++ GameStay/SuccessRegist.aspx.cs
@@ -7,15 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace GameStay
 {
-    public partial class SuccessRegist : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+	public partial class SuccessRegist : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
 
-        }
-        protected void gologin_click(object sender, EventArgs e)
-        {
-            Response.Redirect("Login.aspx");
-        }
-    }
+		}
+	}
 }
